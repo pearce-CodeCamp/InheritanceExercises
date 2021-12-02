9ecbf665f38adeeb0f38c863e40dde6706164b9c
Pearce Odegard <69996490+podegard89@users.noreply.github.com>
2021-12-02 16:47:38 -0600

    Created 3 classes

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Technology
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Computer computer1 = new Computer("HP", 16, "Windows");
+            Console.WriteLine(computer1.GetInfo());
+            Console.WriteLine(computer1.PowerOnOff());
+            Console.WriteLine(computer1.GetInfo());*/
+
+            Laptop macbookAir = new Laptop("Apple", 8, "macOS", false);
+            Console.WriteLine(macbookAir.GetInfo());
+            macbookAir.UpdateMemoryAmount(8);
+            macbookAir.PowerOnOff();
+            Console.WriteLine(macbookAir.GetInfo());
+
+            Smartphone iphone8 = new Smartphone("Apple", 2, "iOS", false);
+            Console.WriteLine(iphone8.GetInfo());
+            Console.WriteLine(iphone8.IsPhoneGood());
+        }
+    }
+}
---
+++ Smartphone.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Technology
+{
+    internal class Smartphone : Computer
+    {
+        public bool HasHeadphoneJack { get; set; }
+
+        public Smartphone(string manufacturer, int memory, string oS, bool hasHeadphone) : base(manufacturer, memory, oS)
+        {
+            HasHeadphoneJack = hasHeadphone;
+        }
+
+        public string IsPhoneGood()
+        {
+            if(HasHeadphoneJack)
+            {
+                return "Nice! This is a good, useful smartphone. :)";
+            } else
+            {
+                return "Bleh! Whyyyyyyy... please Apple?";
+            }
+        }
+    }
+}
